0c7bd1cbdbeae8e7258752a24a599f22a46857b2
Thomas-Eelvelt <teelvelt@worth.systems>
2025-03-17 09:51:24 +0100

    WIP

---
+++ OMC/Infrastructure/Persistence/WebQueries/KTO/Interfaces/IHttpNetworkServiceKto.cs
@@ -0,0 +1,33 @@
+﻿// © 2025, Worth Systems.
+
+using Microsoft.AspNetCore.Http;
+using WebQueries.DataQuerying.Models.Responses;
+using WebQueries.DataSending.Clients.Enums;
+
+namespace WebQueries.KTO.Interfaces
+{
+    /// <summary>
+    /// The service defining basic HTTP Requests contracts (e.g., GET, POST).
+    /// </summary>
+    public interface IHttpNetworkServiceKto
+    {
+        /// <summary>
+        /// Sends request to the given Web API service using a specific <see cref="HttpClient"/>.
+        /// </summary>
+        /// <param name="uri">The URI to be used with <see cref="HttpMethod.Get"/> request.</param>
+        /// <returns>
+        ///   The <see langword="string"/> JSON response from the Web API service.
+        /// </returns>
+        internal Task<HttpRequestResponse> GetAsync(Uri uri);
+
+        /// <summary>
+        /// Posts request to the given Web API service using a specific <see cref="HttpClient"/>.
+        /// </summary>
+        /// <param name="uri">The URI to be used with <see cref="HttpMethod.Post"/> request.</param>
+        /// <param name="jsonBody">The content in JSON format to be passed with <see cref="HttpMethods.Post"/> request as HTTP Request Body.</param>
+        /// <returns>
+        ///   The <see langword="string"/> JSON response from the Web API service.
+        /// </returns>
+        internal Task<HttpRequestResponse> PostAsync(string jsonBody);
+    }
+}
---
+++ OMC/Infrastructure/Persistence/WebQueries/KTO/Models/KtoCustomerObject.cs
@@ -0,0 +1,96 @@
+﻿using System.Text.Json.Serialization;
+using ZhvModels.Mapping.Models.Interfaces;
+
+namespace WebQueries.KTO.Models
+{
+    /// <summary>
+    /// Represents the root object containing customer data.
+    /// </summary>
+    /// <seealso cref="IJsonSerializable"/>
+    public struct KtoCustomerObject : IJsonSerializable
+    {
+        /// <summary>
+        /// The email address retrieved from the KlantAPI.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("emailadres")]
+        [JsonPropertyOrder(1)]
+        public string Emailadres { get; init; }
+
+        /// <summary>
+        /// The transaction date when the final status was created in the ZaakAPI.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("transactiedatum")]
+        [JsonPropertyOrder(2)]
+        public string TransactionDate { get; init; }
+
+        /// <summary>
+        /// The date/time when the request is sent to Expoints.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("sendTime")]
+        [JsonPropertyOrder(3)]
+        public string SendTime { get; init; }
+
+        /// <summary>
+        /// The customer data columns containing survey and service information.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("customerDataColumns")]
+        [JsonPropertyOrder(5)]
+        public CustomerDataColumns Columns { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerDataResponse"/> struct.
+        /// </summary>
+        public KtoCustomerObject()
+        {
+            Emailadres = string.Empty;
+            TransactionDate = string.Empty;
+            SendTime = string.Empty;
+            Columns = new CustomerDataColumns();
+        }
+    }
+
+    /// <summary>
+    /// Represents the customer data columns for surveys and services.
+    /// </summary>
+    /// <seealso cref="IJsonSerializable"/>
+    public struct CustomerDataColumns : IJsonSerializable
+    {
+        /// <summary>
+        /// The name of the survey.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("vragenlijstnaam")]
+        [JsonPropertyOrder(0)]
+        public string SurveyName { get; init; }
+
+        /// <summary>
+        /// The name of the service.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("dienstnaam")]
+        [JsonPropertyOrder(1)]
+        public string ServiceName { get; init; }
+
+        /// <summary>
+        /// The type of measurement.
+        /// </summary>
+        [JsonRequired]
+        [JsonPropertyName("typemeting")]
+        [JsonPropertyOrder(2)]
+        public string SurveyType { get; init; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomerDataColumns"/> struct.
+        /// </summary>
+        public CustomerDataColumns()
+        {
+            SurveyName = string.Empty;
+            ServiceName = string.Empty;
+            SurveyType = string.Empty;
+        }
+    }
+}
