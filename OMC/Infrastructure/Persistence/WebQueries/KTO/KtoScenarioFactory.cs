3d7069048768310473b6bf8e7af6581414ee44da
Thomas Eelvelt <teelvelt@worth.systems>
2025-11-13 13:50:08 +0100

    WIP: New Kto Implementation

---
+++ OMC/Infrastructure/Persistence/WebQueries/KTO/KtoScenarioFactory.cs
@@ -0,0 +1,39 @@
+﻿using WebQueries.DataQuerying.Adapter.Interfaces;
+using WebQueries.DataQuerying.Proxy.Interfaces;
+using WebQueries.KTO.Interfaces;
+using WebQueries.KTO.Models;
+using ZhvModels.Mapping.Models.POCOs.NotificatieApi;
+
+namespace WebQueries.KTO
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class KtoScenarioFactory : IKtoScenarioFactory
+    {
+        private readonly IDataQueryService<NotificationEvent> _dataQuery;
+        private readonly IQueryContext _queryContext;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataQuery"></param>
+        /// <param name="queryContext"></param>
+        public KtoScenarioFactory(
+            IDataQueryService<NotificationEvent> dataQuery,
+            IQueryContext queryContext)
+        {
+            _dataQuery = dataQuery;
+            _queryContext = queryContext;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public KtoScenario Create()
+        {
+            return new KtoScenario(_dataQuery, _queryContext);
+        }
+    }
+}
---
+++ OMC/Infrastructure/Persistence/WebQueries/KTO/Models/KtoScenario.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using WebQueries.DataQuerying.Adapter.Interfaces;
+using WebQueries.DataQuerying.Models.Responses;
+using WebQueries.DataQuerying.Proxy.Interfaces;
+using ZhvModels.Extensions;
+using ZhvModels.Mapping.Models.POCOs.NotificatieApi;
+using ZhvModels.Mapping.Models.POCOs.Objecten.KTO;
+using ZhvModels.Mapping.Models.POCOs.Objecten.Message;
+
+namespace WebQueries.KTO.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class KtoScenario
+    {
+        /// <inheritdoc cref="IQueryContext"/>
+        private IQueryContext QueryContext { get; set; }
+
+        /// <inheritdoc cref="IDataQueryService{TModel}"/>
+        private IDataQueryService<NotificationEvent> DataQuery { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KtoScenario"/> class.
+        /// </summary>
+        public KtoScenario(
+            IDataQueryService<NotificationEvent> dataQuery,
+            IQueryContext queryContext)
+        {
+            DataQuery = dataQuery;
+            QueryContext = queryContext;
+        }
+
+        /// <summary>
+        /// Checks if there are KTO settings, if so tries to send a request to KTO Service.
+        /// </summary>
+        /// <param name="notification"></param>
+        /// <exception cref="ArgumentException"></exception>
+        /// <returns>
+        ///   The JSON content for HTTP Request Body.
+        /// </returns>
+        public async Task<HttpRequestResponse> SendKtoAsync(NotificationEvent notification)
+        {
+            QueryContext = DataQuery.From(notification);
+
+            MessageObject messageObject = await QueryContext.GetMessageAsync();
+            KtoObject response = await QueryContext.GetKtoObjectAsync(notification.MainObjectUri.GetGuid());
+            string ktoMessage = response.Record.Data.KtoMessage;
+
+            if (string.IsNullOrEmpty(ktoMessage))
+            {
+#pragma warning disable CA2208
+                throw new ArgumentException(@"KTO object cannot be null or empty.", nameof(ktoMessage));
+#pragma warning restore CA2208
+            }
+
+            string ktoPayload = JsonSerializer.Serialize(ktoMessage);
+            HttpRequestResponse ktoResult = await SendKtoRequestAsync(ktoPayload);
+
+            if (ktoResult.IsFailure)
+            {
+                throw new HttpRequestException("Failed to send KTO request. JSonMessage: " + ktoResult.JsonResponse);
+            }
+
+            HttpRequestResponse resultRemoveObjectResponse = await RemoveKtoObjectAsync(notification.MainObjectUri.GetGuid());
+
+            if (resultRemoveObjectResponse.IsFailure)
+            {
+                throw new HttpRequestException("Failed to set kto object to null. Kto has been sent successfully." +
+                                               " JSonMessage: " + resultRemoveObjectResponse.JsonResponse);
+            }
+
+            return ktoResult;
+        }
+
+        #region Helpers
+        private async Task<HttpRequestResponse> SendKtoRequestAsync(string ktoPayload)
+        {
+            return await QueryContext.SendKtoAsync(ktoPayload);
+        }
+
+        private async Task<HttpRequestResponse> RemoveKtoObjectAsync(Guid ktoObject)
+        {
+            return await QueryContext.DeleteObjectAsync(ktoObject);
+        }
+        #endregion
+    }
+}
