06b4ac814049d0005e98d502ad16d27dd18231aa
Thomas Eelvelt <teelvelt@worth.systems>
2026-04-29 11:24:16 +0200

    WIP-POC MijnOverheid CloudEvent

---
+++ OMC/Infrastructure/Persistence/WebQueries/MijnOverheid/Interfaces/IMijnOverheidClient.cs
@@ -0,0 +1,17 @@
+﻿using WebQueries.MijnOverheid.Models;
+
+namespace WebQueries.MijnOverheid.Interfaces
+{
+    /// <summary>
+    /// Client for sending CloudEvents to MijnOverheid.
+    /// </summary>
+    public interface IMijnOverheidClient
+    {
+        /// <summary>
+        /// Sends a CloudEvent to MijnOverheid.
+        /// </summary>
+        /// <param name="cloudEvent">The event to send.</param>
+        /// <returns>True if the request succeeded (2xx status), otherwise false.</returns>
+        Task<MijnOverheidResponse> SendAsync(CloudEvent cloudEvent);
+    }
+}
